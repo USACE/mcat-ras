65c3021f446e5ae2722cad565320e7e1f19d3acf
Abdul Raheem Siddiqui <abdulraheemsiddiqui@gmail.com>
2022-01-21 08:36:29 -0500

    Make connection name a key

--- tools/connections.go
+++ tools/connections.go
@@ -9,11 +9,10 @@ import (
 )
 
 // Store HEC-RAS SA/2D Area Connections
-type connections struct {
-	Name        string      `json:"Connection Name"`
-	Description string      `json:"Connection Description"`
-	UpSA        string      `json:"Connection Up SA"`
-	DnSA        string      `json:"Connection Dn SA"`
+type connection struct {
+	Description string      `json:"Description"`
+	UpSA        string      `json:"Up SA"`
+	DnSA        string      `json:"Dn SA"`
 	WeirWidth   float64     `json:"Weir Width"`
 	WeirElev    maxMinPairs `json:"Weir Elevations"`
 	NumGates    int         `json:"Num Gates"`
@@ -23,12 +22,13 @@ type connections struct {
 }
 
 // Extract data from Connections
-func getConnectionsData(rm *RasModel, fn string, i int) (connections, error) {
-	connection := connections{}
+func getConnectionsData(rm *RasModel, fn string, i int) (string, connection, error) {
+	name := ""
+	connection := connection{}
 
 	f, err := rm.FileStore.GetObject(fn)
 	if err != nil {
-		return connection, errors.Wrap(err, 0) 
+		return name, connection, errors.Wrap(err, 0) 
 	}
 	defer f.Close()
 
@@ -39,16 +39,15 @@ func getConnectionsData(rm *RasModel, fn string, i int) (connections, error) {
 		ci++
 		if ci == i {
 			lineData := strings.Split(rightofEquals(cSc.Text()), ",")
-			name := strings.TrimSpace(lineData[0])
-
-			connection.Name = name
+			name = strings.TrimSpace(lineData[0])
 		} else if ci > i {
 			line := cSc.Text()
 			switch {
+				
 			case strings.HasPrefix(line, "Connection Desc="):
 				description, err := getDescriptionConnections(cSc, "Connection Line=")
 				if err != nil {
-					return connection, errors.Wrap(err, 0) 
+					return name, connection, errors.Wrap(err, 0) 
 				}
 				connection.Description += description
 
@@ -61,20 +60,20 @@ func getConnectionsData(rm *RasModel, fn string, i int) (connections, error) {
 			case strings.HasPrefix(line, "Conn Weir WD="):
 				weirWidth, err := strconv.ParseFloat(rightofEquals(line), 64)
 				if err != nil {
-					return connection, errors.Wrap(err, 0) 
+					return name, connection, errors.Wrap(err, 0) 
 				}
 				connection.WeirWidth = weirWidth
 
 			case strings.HasPrefix(line, "Conn Weir SE="):
 				nElev, err := strconv.Atoi(rightofEquals(line))
 				if err != nil {
-					return connection, errors.Wrap(err, 0) 
+					return name, connection, errors.Wrap(err, 0) 
 				}
 				nLines := numberofLines(nElev*2, 80, 8)
 
 				elev, _, err := getMaxMinElev(cSc, 0, nLines, 0, 80, 8, 2)
 				if err != nil {
-					return connection, errors.Wrap(err, 0) 
+					return name, connection, errors.Wrap(err, 0) 
 				}
 				connection.WeirElev = elev
 
@@ -82,7 +81,7 @@ func getConnectionsData(rm *RasModel, fn string, i int) (connections, error) {
 				cSc.Scan()
 				gate, err := getGates(cSc.Text())
 				if err != nil {
-					return connection, errors.Wrap(err, 0) 
+					return name, connection, errors.Wrap(err, 0) 
 				}
 				connection.Gates = append(connection.Gates, gate)
 				connection.NumGates++
@@ -90,16 +89,20 @@ func getConnectionsData(rm *RasModel, fn string, i int) (connections, error) {
 			case strings.HasPrefix(line, "Connection Culv="):
 				conduit, err := getConduits(line, false)
 				if err != nil {
-					return connection, errors.Wrap(err, 0) 
+					return name, connection, errors.Wrap(err, 0) 
 				}
 				connection.Conduits = append(connection.Conduits, conduit)
 				connection.NumConduits++
 
 			case strings.HasPrefix(line, "Conn Outlet Rating Curve="):
-				return connection, nil
-
+				return name, connection, nil
+				
+			case strings.HasPrefix(line, "Connection="):
+				// guard to make sure new Connection don't overwrite previous values
+				// return with whatever data is available
+				return name, connection, nil
 			}
 		}
 	}
-	return connection, nil
+	return name, connection, nil
 }
